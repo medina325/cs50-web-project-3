c6e35d78da4f07b76feba5cefc59f39f70e32b41
Gabriel Medina Braga <medina_cdz@hotmail.com>
2020-11-12 01:53:21 -0300

    Lista de e-mails sendo feita

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -24,6 +24,8 @@ function compose_email() {
   // If the submit button is hit, then sends the e-mail
   document.querySelector('#compose-form').onsubmit = () => {
 
+    // if the fetch is successful then the response variable is gonna contain the data returned from the API
+    // 'then' is gonna return that data in json format which will be in the result variable
     fetch('/emails', {
       method: 'POST',
       body: JSON.stringify({
@@ -45,11 +47,37 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+  console.log(mailbox);
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Query for all e-mails that are in the mailbox given
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+    
+    const ul = document.createElement('ul');
+    ul.className = 'list-group';
+
+    emails.forEach((email) => {
+      const a = document.createElement('a');
+      a.href = '#';
+      
+      if(email.read === false)
+        a.className = 'list-group-item list-group-item-action';
+      else
+        a.className = 'list-group-item list-group-item-action disabled';
+
+      a.innerHTML = email.subject + " " + email.sender + email.timestamp;
+      document.querySelector('#emails-view').append(a);
+    });
+    
+
+
+  });
 }
